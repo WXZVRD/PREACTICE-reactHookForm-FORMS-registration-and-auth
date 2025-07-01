e67692c19a57aa44228a738678e07a9aedba522d
wxzvd <forworknickworron@gmail.com>
2025-07-01 18:23:45 +0500

    Project init

--- edu-reactforms/src/app/auth/authorization/page.tsx
+++ edu-reactforms/src/app/auth/authorization/page.tsx
@@ -1,6 +1,6 @@
 import {AuthForm} from "@/features/auth/authorization/ui/AuthForm";
 
-export default function RegistrationPage() {
+export default function AuthPage() {
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
             <div className="max-w-md w-full bg-white p-6 rounded-xl shadow-xl">
